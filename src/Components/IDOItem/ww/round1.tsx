07c95d7fd521a796fc29cc76afd70f92d2fae36c
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-06-11 17:13:55 +0800

    fixes

--- src/Components/IDOItem/ww/round1.tsx
+++ src/Components/IDOItem/ww/round1.tsx
@@ -300,8 +300,8 @@ const _MRLIDO = (props: { IDOParams, name }) => {
                                             </div>
                                         </div>
 
-                                        <div className={"bg-warning-200/30 rounded-xl p-2"}>
-                                            <span className={"text-warning-500 flex flex-row items-center justify-center gap-2"}>
+                                        <div className={"bg-success-200/30 rounded-xl p-2"}>
+                                            <span className={"text-success-500 flex flex-row items-center justify-center gap-2"}>
                                                 <svg
                                                     className="animate-spin h-5 w-5 text-current"
                                                     fill="none"
@@ -322,7 +322,7 @@ const _MRLIDO = (props: { IDOParams, name }) => {
                                                         fill="currentColor"
                                                     />
                                                 </svg>
-                                                {"UPCOMING"}
+                                                {"LIVE"}
                                             </span>
                                         </div>
 
