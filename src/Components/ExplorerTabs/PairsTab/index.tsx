b237d67d21e80fb1e1182171aace20d3eec0c1e1
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-07-20 02:56:05 +0900

    fixes

--- src/Components/ExplorerTabs/PairsTab/index.tsx
+++ src/Components/ExplorerTabs/PairsTab/index.tsx
@@ -301,6 +301,136 @@ const _PairTAB = (props: { exchange, tokens }) => {
         setLoaded(true);
     }
 
+
+    const fetchKayenPairs = async () => {
+        console.log("chilizswap")
+        if (chainId != ChainId.CHILIZ_MAINNET) {
+            return
+        }
+        const jalaPairList = []
+        const pairLength = (await JALASWAP.allPairsLength()).toNumber()
+        console.log("JALA PAIR LENGTH",pairLength)
+
+        for (let i = 0; i < pairLength; i++) {
+            const pair = await JALASWAP.allPairs(i);
+            console.log(`index`, pair)
+
+            let pairContract = PAIRContract(pair);
+
+            let token0 = await pairContract.token0();
+            let token1 = await pairContract.token1()
+
+            const [_reserve0, _reserve1, _blockTimestampLast] = await pairContract.getReserves();
+
+
+
+
+            let token0ERC = ERC20Contract(token0);
+            let token1ERC = ERC20Contract(token1);
+
+            let abiERC = [
+                'function name() external view returns (string memory)',
+                'function symbol() external view returns (string memory)',
+                'function decimals() external view returns (uint256)',
+            ];
+
+            let abiInterfaceParam = new ethers.utils.Interface(abiERC)
+
+            let multicallParams = [
+                {
+                    target: token0,
+                    callData: abiInterfaceParam.encodeFunctionData("name", [])
+                },
+                {
+                    target: token0,
+                    callData: abiInterfaceParam.encodeFunctionData("symbol", [])
+                },
+                {
+                    target: token0,
+                    callData: abiInterfaceParam.encodeFunctionData("decimals", [])
+                },
+                {
+                    target: token1,
+                    callData: abiInterfaceParam.encodeFunctionData("name", [])
+                },
+                {
+                    target: token1,
+                    callData: abiInterfaceParam.encodeFunctionData("symbol", [])
+                },
+                {
+                    target: token1,
+                    callData: abiInterfaceParam.encodeFunctionData("decimals", [])
+                },
+            ];
+
+            let abiERCResults = [
+                { "type": 'string memory', "variable": "name" },
+                { "type": 'string memory', "variable": "symbol" },
+                { "type": 'uint256', "variable": "decimals" },
+                { "type": 'string memory', "variable": "name" },
+                { "type": 'string memory', "variable": "symbol" },
+                { "type": 'uint256', "variable": "decimals" },
+            ];
+
+            var base = {
+                name: "",
+                symbol: "",
+                decimals: 0
+            }
+            var quote = {
+                name: "",
+                symbol: "",
+                decimals: 0
+            }
+
+            try {
+                const [blockNum, multicallResult] = await EXCHANGE.callStatic.aggregate(multicallParams)
+
+
+                base.name = ethers.utils.defaultAbiCoder.decode(["string memory name"], multicallResult[0]).name
+                base.symbol = ethers.utils.defaultAbiCoder.decode(["string memory symbol"], multicallResult[1]).symbol
+                base.decimals = ethers.utils.defaultAbiCoder.decode(["uint256 decimals"], multicallResult[2]).decimals
+
+                quote.name = ethers.utils.defaultAbiCoder.decode(["string memory name"], multicallResult[3]).name
+                quote.symbol = ethers.utils.defaultAbiCoder.decode(["string memory symbol"], multicallResult[4]).symbol
+                quote.decimals = ethers.utils.defaultAbiCoder.decode(["uint256 decimals"], multicallResult[5]).decimals
+
+
+
+            } catch (e) {
+                console.log("multicallException:", e)
+                //setLoaded(false)
+                //return
+            }
+
+
+            const tokenA = new Token(chainId, token0, BigNumber.from(base.decimals).toNumber())
+            const tokenB = new Token(chainId, token1, BigNumber.from(quote.decimals).toNumber())
+            const price = new Price(tokenA, tokenB, _reserve0, _reserve1)
+
+            const canInvertPrice = Boolean(
+                price && price.baseCurrency && price.quoteCurrency && !price.baseCurrency.equals(price.quoteCurrency))
+
+            const _basePrice = price?.toSignificant(6)
+            const _quotePrice = canInvertPrice ? price?.invert()?.toSignificant(6) : "-"
+            let pairInfo = {
+                base: base,
+                quote: quote,
+                token0: token0,
+                token1: token1,
+                pair: pair,
+                reserveBase: _reserve0,
+                reserveQuote: _reserve1,
+                basePrice: _basePrice,
+                quotePrice: _quotePrice,
+            }
+
+            jalaPairList.push(pairInfo)
+        }
+        setAllExchangePairs(jalaPairList);
+        setLoaded(true);
+    }
+
     useEffect(() => {
 
 
@@ -308,7 +438,7 @@ const _PairTAB = (props: { exchange, tokens }) => {
             fetchIMONPairs();
         } else if (props.exchange === "JALA") {
             console.log("jalaswap")
-            fetchJALAPairs();
+            fetchKayenPairs();
         } else if (props.exchange === "CHILIZSWAP") {
             console.log("chilizswap")
             fetchChilizSwapPairs();
