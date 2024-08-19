26b5139ca5184a6e3cd3baef8d2b765048fcfc23
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-08-19 18:15:51 +0900

    fixes

--- src/Components/EarnTabs/Earn/index.tsx
+++ src/Components/EarnTabs/Earn/index.tsx
@@ -111,7 +111,7 @@ const _EARN_TAB = () => {
 
 
     function isAllowedAsset(asset: any): boolean {
-        const wrappedSymbols = ['CHZINU','KWL','FAFO','DSWAP','CHSQUAD','FANFAN'];
+        const wrappedSymbols = ['CHZINU','KWL','FAFO','DSWAP','CHSQUAD','FANFAN','SUCK'];
         return wrappedSymbols.includes(asset.toString().toUpperCase());
     }
 
