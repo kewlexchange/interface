e02c31269ac8ecc8399f5d0b9ac3af2567b19288
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-09-26 07:03:06 +0700

    fixes

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -63,7 +63,7 @@ const HomePage: React.FunctionComponent<IPage> = props => {
 
                                             <div className=" w-full h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
                                             <h1 className="flex flex-col items-center justify-center z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-                                                <div className="w-full sm:text-7xl whitespace-nowrap text-3xl my-5 homeshimmer"><span className={"font-bold"}>KEWL.</span>EXCHANGE</div>
+                                                <div className="w-full sm:text-6xl whitespace-nowrap text-3xl my-5 homeshimmer"><span className={"font-bold"}>KEWL.</span>EXCHANGE</div>
 
                                             </h1>
 
