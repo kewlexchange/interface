5630d69bcec2708cf9fe19bb2fc1590c582d68f3
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-12-10 14:31:56 +0700

    fixes

--- src/pages/SwapPage/index.tsx
+++ src/pages/SwapPage/index.tsx
@@ -1,7 +1,7 @@
 import IPage from "../../interfaces/page";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Tabs, Tab, Card, CardBody, CardFooter, Button } from "@nextui-org/react";
+import { Tabs, Tab, Card, CardBody, CardFooter, Button, Link } from "@nextui-org/react";
 import Identicon from "../../Components/Identicon";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { SWAP_TAB } from "../../Components/SwapComponents/Swap";
@@ -13,16 +13,70 @@ import { BURN_TAB } from "../../Components/MetamorphTabs/BurnTab";
 import { WRAP_TAB } from "../../Components/SwapComponents/Wrap";
 import { TRADE_TAB } from "../../Components/SwapComponents/Trade";
 import { ARBITRAGE_TAB } from "../../Components/SwapComponents/Arbitrage";
+import { useSocket } from "../../hooks/useSocketProvider";
+import { generateTxLink } from "../../utils/web3Provider";
 
 
 const SwapPage: React.FunctionComponent<IPage> = props => {
     const { pathname } = useLocation();
     const { connector, account, provider, chainId } = useWeb3React()
+    const { socket } = useSocket();
 
     useEffect(() => {
         document.title = props.title + " - KEWL EXCHANGE";
     }, [props, chainId, account]);
 
+   
+    const TradeHistory = () => {
+        const [trade, setTrade] = useState(null)
+
+        const initChatSocket = () => {
+            if (!socket) {
+                return
+            }
+            socket.on("trade", (data: any) => {
+                //setMessages(messages => [...messages, JSON.parse(data)]);
+                const parsedData = JSON.parse(data);
+                setTrade(parsedData)
+                console.log("data,",parsedData)
+    
+            });
+        }
+        useEffect(() => {
+            if (socket) {
+                initChatSocket()
+            }
+    
+        }, [socket])
+
+        return <div className="w-full">
+            <div className="w-full">Latest Trade Info</div>
+            <div className="w-full grid grid-cols-2">
+                <span>DEX</span>
+                <span>{trade?.DEX}</span>
+            </div>
+            <div className="w-full grid grid-cols-2">
+                <span>Pair</span>
+                <span>{trade?.baseTokenSymbol} {trade ? "x" : ""} {trade?.quoteTokenSymbol}</span>
+            </div>
+            <div className="w-full grid grid-cols-2">
+                <span>Input</span>
+                <span>{trade?.baseAmount} {trade?.baseTokenSymbol}</span>
+            </div>
+            <div className="w-full grid grid-cols-2">
+                <span>Output</span>
+                <span>{trade?.quoteAmount} {trade?.quoteTokenSymbol}</span>
+            </div>
+            <div className="w-full grid grid-cols-2">
+                <span>Price</span>
+                <span>{trade ? parseFloat(trade?.price).toFixed(4) : ""} {trade?.baseTokenSymbol}</span>
+            </div>
+            <div className="w-full grid grid-cols-2">
+                <span>Proof</span>
+                <span> {trade ? <Button target="_blank" size="sm" href={generateTxLink(trade?.chainId, trade?.txHash)} as={Link} >View on Explorer</Button> : ""}</span>
+            </div>
+        </div>
+    }
 
     return (
 
@@ -105,6 +159,9 @@ const SwapPage: React.FunctionComponent<IPage> = props => {
                                 </Tabs>
                             </div>
                         </CardBody>
+                        <CardFooter>
+                            <TradeHistory/>
+                        </CardFooter>
                    
                     </Card>
                 </div>
