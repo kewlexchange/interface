d1f4e6e1e62047aa3353c3557ebb442ee64dff08
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-03-10 07:08:16 +0800

    fixes'

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -201,9 +201,17 @@ export const DECENTRALIZED_EXCHANGES = [
         router:"0x1570300e9cFEC66c9Fb0C8bc14366C86EB170Ad0",
         weth:"0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38",
         flag:false
+    },
+
+    {
+        logo:"/images/dex/swapx.svg",
+        chainId:146,
+        dex:"SwapX",
+        router:"0x05c1be79d3aC21Cc4B727eeD58C9B2fF757F5663",
+        weth:"0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38",
+        flag:true
     }
 
-    
 
     
 
